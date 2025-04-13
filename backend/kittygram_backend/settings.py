746f914d299cce57e657dca99bc5ac53998c40b3
Anton K <executor1@gmail.com>
2025-04-14 02:01:22 +0300

    sfsda

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,8 +11,8 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['158.160.3.109',
-                 'kittygram777.zapto.org',
+ALLOWED_HOSTS = ['kittygram777.zapto.org',
+                 '158.160.3.109',
                  '127.0.0.1',
                  'localhost'
                  ]
