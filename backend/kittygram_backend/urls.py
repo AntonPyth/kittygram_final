1f8de5aca3294158354586f3d78253a241d14429
Anton K <executor1@gmail.com>
2025-04-14 00:53:54 +0300

    fixing bugs

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,3 +1,26 @@
+# from cats.views import AchievementViewSet, CatViewSet
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib import admin
+# from django.urls import include, path
+# from rest_framework import routers
+# 
+# router = routers.DefaultRouter()
+# router.register(r'cats', CatViewSet)
+# router.register(r'achievements', AchievementViewSet)
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include(router.urls)),
+#     path('api/', include('djoser.urls')),  # Работа с пользователями
+#     path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
+    
+
 from cats.views import AchievementViewSet, CatViewSet
 from django.conf import settings
 from django.conf.urls.static import static
@@ -12,8 +35,8 @@ router.register(r'achievements', AchievementViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/', include('djoser.urls')),  # Работа с пользователями
-    path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
+    path('api/users/', include('djoser.urls')),  # Работа с пользователями
+    path('api/users/', include('djoser.urls.authtoken')),  # Работа с токенами
 ]
 
 if settings.DEBUG:
