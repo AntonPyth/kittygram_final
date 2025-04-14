e6ac95e6dc84654ab7c01d8652d65769dceb3ab7
Anton K <executor1@gmail.com>
2025-04-14 03:40:26 +0300

    tryyyн

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from djoser.views import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
@@ -12,9 +13,19 @@ router.register(r'achievements', AchievementViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/users/', include('djoser.urls')),  # Работа с пользователями
-    path('api/users/', include('djoser.urls.authtoken')),  # Работа с токенами
+    # Регистрация пользователя
+    path('api/users/register/', UserViewSet.as_view({'post': 'create'}), name='user-register'),
+    # Токены
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include(router.urls)),
+#     path('api/users/', include('djoser.urls')),
+#     # path('api/users/', include('djoser.urls')),  # Работа с пользователями
+#     # path('api/users/', include('djoser.urls.authtoken')),  # Работа с токенами
+# ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
