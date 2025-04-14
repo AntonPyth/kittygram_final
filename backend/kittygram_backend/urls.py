bc81347d1382fc05b4e57ee75ca1d8e2d3e56038
Anton K <executor1@gmail.com>
2025-04-14 03:43:52 +0300

    tryyyн

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,10 +1,10 @@
 from cats.views import AchievementViewSet, CatViewSet
+from djoser.views import UserViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from djoser.views import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
@@ -14,7 +14,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     # Регистрация пользователя
-    path('api/users/register/', UserViewSet.as_view({'post': 'create'}), name='user-register'),
+    path(
+        'api/users/register/',
+        UserViewSet.as_view({'post': 'create'}),
+        name='user-register'
+        ),
     # Токены
     path('api/auth/', include('djoser.urls.authtoken')),
 ]
